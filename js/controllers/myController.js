36103d9c53b5f400bf1a59bb1f9c76df1dcd32c1
John Ryan <jcryan85@gmail.com>
2014-03-10 10:51:56 -0700

    added comments

--- js/controllers/myController.js
+++ js/controllers/myController.js
@@ -2,6 +2,7 @@ app.controller('myController', function($scope){
 	$scope.todo = '';
 	$scope.list = ['Learn Angular'];
 
+	/* addToList function on myController */
 	$scope.addToList = function() {
 		$scope.list.push($scope.todo); 
 		$scope.list; 
